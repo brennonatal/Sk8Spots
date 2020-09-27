a1547ab4484d5c0198ea36234e8b5448058c0e1e
Brenno Natal <natalbrenno@gmail.com>
2020-09-27 18:34:17 -0300

    Cleanning map

--- client/public/map.js
+++ client/public/map.js
@@ -1,8 +1,10 @@
 let map;
 
-function initMap(){
-          map = new google.maps.Map(document.getElementById("map"), {
-            center: { lat: -34.397, lng: 150.644 },
-            zoom: 8
-          });
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 10,
+        disableDefaultUI: true,
+        gestureHandling: "cooperative",
+    });
 }
